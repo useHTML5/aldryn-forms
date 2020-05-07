62a51707e6e859647c7b090c9c90d8487700606f
useHTML5 <mne@sait-nado.ru>
2020-05-07 15:17:46 +0500

    add form prefix

--- aldryn_forms/views.py
+++ aldryn_forms/views.py
@@ -18,7 +18,7 @@ class AjaxSubmit(FormView):
     # template_name = 'aldryn_forms/form.html'
 
     def post(self, request, *args, **kwargs):
-        page_path = request.POST.get('page_path', '').strip('/')
+        page_path = request.POST.get('form_page_path', '').strip('/')
         cms_page = get_page_from_path(get_current_site(request), page_path)
         if not cms_page:
             return HttpResponseBadRequest('no cms_page')
